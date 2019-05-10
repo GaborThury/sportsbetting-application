75a85151e509d9932590a2ff69efb1af57b3c19e
Gabor Thury <Gabor_Thury@epam.com>
2019-05-10 10:01:48 +0200

    logging

--- src/main/java/com/epam/training/sportsbetting/aspect/AppAspect.java
+++ src/main/java/com/epam/training/sportsbetting/aspect/AppAspect.java
@@ -1,9 +1,12 @@
 package com.epam.training.sportsbetting.aspect;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,18 +17,21 @@ public class AppAspect {
 
     private long startTime;
 
+    @Autowired
+    private Logger logger;
+
     @Before("within(com.epam.training.sportsbetting.service..*)")
     public void before(JoinPoint joinPoint) {
         startTime = System.currentTimeMillis();
-        System.out.print("Calling method: " + joinPoint.getSignature().getName());
-        System.out.println(" with parameters: " + Arrays.toString(joinPoint.getArgs()));
+        logger.info("Calling method: " + joinPoint.getSignature().getName());
+        logger.info(" with parameters: " + Arrays.toString(joinPoint.getArgs()));
     }
-    
+
     @AfterReturning(pointcut = "within(com.epam.training.sportsbetting.service..*)",
         returning = "returnValue")
     public void after(Object returnValue) {
-        System.out.println("The return value of the method is: " + returnValue);
+        logger.info("The return value of the method is: " + returnValue);
         long executionTime = System.currentTimeMillis() - startTime;
-        System.out.println("Method took " + executionTime + " ms to execute.");
+        logger.info("Method took " + executionTime + " ms to execute.");
     }
 }
--- src/main/java/com/epam/training/sportsbetting/configuration/AppConfiguration.java
+++ src/main/java/com/epam/training/sportsbetting/configuration/AppConfiguration.java
@@ -1,8 +1,11 @@
 package com.epam.training.sportsbetting.configuration;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Scope;
 
 import java.util.Scanner;
 
@@ -15,6 +18,12 @@ public class AppConfiguration {
     @Bean
     public Scanner scanner() {
         return new Scanner(System.in);
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Logger logger(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass());
     }
 
 }
