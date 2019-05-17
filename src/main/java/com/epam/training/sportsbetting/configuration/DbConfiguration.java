a250c4ab2db4e862e7d6d4040044c14df54a6d8c
Gabor Thury <Gabor_Thury@epam.com>
2019-05-17 13:33:12 +0200

    player id auto generate

--- src/main/java/com/epam/training/sportsbetting/configuration/DbConfiguration.java
+++ src/main/java/com/epam/training/sportsbetting/configuration/DbConfiguration.java
@@ -50,7 +50,7 @@ public class DbConfiguration {
         return dataSource;
     }
 
-    Properties additionalProperties() {
+    private Properties additionalProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
         //properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
--- src/main/java/com/epam/training/sportsbetting/domain/Player.java
+++ src/main/java/com/epam/training/sportsbetting/domain/Player.java
@@ -16,7 +16,7 @@ import java.time.LocalDate;
 @Table(name = "PLAYER")
 public class Player extends User {
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column
